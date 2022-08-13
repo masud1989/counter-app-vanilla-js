d7aa80002e1269e9746c23944ea86aea705ab321
masud1989 <mranasrj@gmail.com>
2022-08-13 10:59:11 +0600

    counter-two

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ decrementEl.addEventListener('click', ()=> {
     count--
     counterEl.innerText = count;
 });
+
+
+
+
+                                          //  Counter2 
+// ----------------------------------------------------------------------------------------------
+// Select Dom Element 
+const counterEl2 = document.getElementById('counter2');
+const incrementEl2 = document.getElementById('increment2');
+const decrementEl2 = document.getElementById('decrement2');
+
+// Set Initial State
+let count2 = 0;
+
+
+// Set EventListener 
+incrementEl2.addEventListener('click', ()=> {
+    count2++
+    counterEl2.innerText = count2;
+});
+
+decrementEl2.addEventListener('click', ()=> {
+    count2--
+    counterEl2.innerText = count2;
+});
